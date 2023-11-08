5ffe0513a1831a5b75712a2f510378377cc7ec49
marianafdo <142455911+marianafdo@users.noreply.github.com>
2023-11-08 20:23:55 -0300

    Add files via upload

--- srvContatos.js
+++ srvContatos.js
@@ -11,12 +11,26 @@ srv.get('/contatos/html', (req,res)=>{
     listaHTML = `<ul>`;
     for (let index = 0; index < contatos.length; index++) {
         const contato = contatos[index];
-    listaHTML= listaHTML + `<li>${contato.nome} - ${contato.email}</li>`    
+        listaHTML= listaHTML + `<li>${contato.nome} - ${contato.email}</li>`    
     }
     listaHTML = listaHTML + `</ul>`;
     res.send(listaHTML);
 })
 
+srv.get('/contatos/csv', (req,res)=>{
+    listaTXT = 'nome,email\n';
+    for (let index = 0; index < contatos.length; index++) {
+        const contato = contatos[index];
+        listaTXT = listaTXT + `${contato.nome}, ${contato.email}` + `\n`
+    }
+    res.send(listaTXT);
+})
+// srv.get('/dizoi/:nome', ...)
+srv.get('/:nome', (req, res)=>{
+    res.send(
+        `Oi, ${req.params.nome}`
+    );
+})
 srv.listen(8080, ()=>{
     console.log('Pronto para servir contatos');
 });
